efb3e29fa678fe922491a0bfd8756e5d2c69fca4
Jordan Vrij <jordan.vrij@ns.nl>
2025-02-14 15:51:37 +0100

    fix

---
+++ Functions/Find-WinEvent.ps1
@@ -0,0 +1,6 @@
+﻿function Find-WinEvent ($KeyWord, [int]$MaxEvents = '100') {
+    Get-WinEvent -ListLog * -EA SilentlyContinue |
+        Where-Object { $_.recordcount -AND $_.lastwritetime -gt [datetime]::today } |
+        ForEach-Object { Get-WinEvent -LogName $_.logname -MaxEvents $MaxEvents | Where-Object Message -Match $KeyWord } |
+        Format-Table TimeCreated, ID, ProviderName, LevelDisplayName, Message -AutoSize -Wrap
+}
--- Functions/Seach-WinEvent.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Search = 'OneDrive'
-
-Get-WinEvent -ListLog * -EA SilentlyContinue |
-Where-Object { $_.recordcount -AND $_.lastwritetime -gt [datetime]::today} |
-ForEach-Object { Get-WinEvent -LogName $_.logname -MaxEvents 100 | Where-Object Message -Match $Search } |
-Format-Table TimeCreated, ID, ProviderName, LevelDisplayName, Message -AutoSize –Wrap
