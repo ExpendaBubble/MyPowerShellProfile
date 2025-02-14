d420df52ec0feb76e55025610f5bbaed0d9b1f94
Jordan Vrij <jordan.vrij@ns.nl>
2025-02-14 15:56:43 +0100

    encoding

--- Functions/Find-WinEvent.ps1
+++ Functions/Find-WinEvent.ps1
@@ -1,4 +1,4 @@
-﻿function Find-WinEvent ($KeyWord, [int]$MaxEvents = '100') {
+function Find-WinEvent ($KeyWord, [int]$MaxEvents = '100') {
     Get-WinEvent -ListLog * -EA SilentlyContinue |
         Where-Object { $_.recordcount -AND $_.lastwritetime -gt [datetime]::today } |
         ForEach-Object { Get-WinEvent -LogName $_.logname -MaxEvents $MaxEvents | Where-Object Message -Match $KeyWord } |
