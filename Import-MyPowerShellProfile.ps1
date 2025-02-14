aa0dba09f5e1da7d877a7f5a69694860714142bb
Jordan Vrij <jordan.vrij@ns.nl>
2025-02-14 14:12:03 +0100

    fix?

--- Import-MyPowerShellProfile.ps1
+++ Import-MyPowerShellProfile.ps1
@@ -27,7 +27,6 @@ Write-Host ""
 Write-Host "LOADING MY FUNCTIONS.. " -ForegroundColor Magenta -NoNewline
 
 # Invoking functions
-Write-Host "RepoUrl: $RepositoryUrl"
 Invoke-Expression (Invoke-WebRequest -Uri "$RepositoryUrl/Functions/Get-WindowsAppFromRegistry.ps1").Content
 
 Write-Host "COMPLETE" -BackgroundColor Magenta
