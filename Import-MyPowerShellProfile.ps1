3da2e8d6e17253b945d0b83acac0aa96b821477d
Jordan Vrij <jordan.vrij@ns.nl>
2025-02-14 15:04:54 +0100

    recursive

--- Import-MyPowerShellProfile.ps1
+++ Import-MyPowerShellProfile.ps1
@@ -1,5 +1,8 @@
 # Variables
-$RepositoryUrl = 'https://raw.githubusercontent.com/ExpendaBubble/MyPowerShellProfile/refs/heads/main'
+$ApiReposUrl = 'https://api.github.com/repos'
+$Owner = 'ExpendaBubble'
+$Repository = 'MyPowerShellProfile'
+$RawUrl = 'https://raw.githubusercontent.com/ExpendaBubble/MyPowerShellProfile/refs/heads/main'
 
 Write-Host ""
 Write-Host "⠀⠀⠀⠀⠀⠑⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Magenta
@@ -27,6 +30,9 @@ Write-Host ""
 Write-Host "LOADING MY FUNCTIONS.. " -ForegroundColor Magenta -NoNewline
 
 # Invoking functions
-Invoke-Expression (Invoke-WebRequest -Uri "$RepositoryUrl/Functions/Get-WindowsAppFromRegistry.ps1").Content
+$request = Invoke-RestMethod "$ApiReposUrl/$Owner/$Repository/git/trees/main?recursive=true"
+$request.tree | Where-Object { $_.type -eq 'blob' -and $_.path -ne 'Import-MyPowerShellProfile.ps1' } | ForEach-Object {
+    Invoke-Expression (Invoke-WebRequest -Uri "$RepositoryUrl/$($_.path)").Content
+}
 
 Write-Host "COMPLETE" -BackgroundColor Magenta
